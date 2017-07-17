086bd88460afa8e1d5d0cb7d332833440f1d067b
Till Zickmantel <zickmantel@physik.uni-luebeck.de>
2017-07-17 09:10:54 +0200

    Added /sim containing all necessary infrastructure for simulator components

---
+++ sim/headers/sim_util.hpp
@@ -0,0 +1,79 @@
+#ifndef SIM_UTIL_H
+#define SIM_UTIL_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+
+#include "getopt_pp.hpp"
+#include "json.hpp"
+
+#define CONST_C 299792458.0
+#define CONST_H 6.62607004e-34
+#define CONST_NA 6.022140857e+23
+#define CONST_PI 3.141592653589793
+
+using json = nlohmann::json;
+
+namespace sim{
+
+    namespace log{
+    
+        void info(std::string msg);
+        void warn(std::string msg);
+        void critical(std::string msg);
+
+    };
+
+    namespace io{
+
+        typedef struct{
+            int16_t x;
+            int16_t y;
+            int16_t z;
+        } coordinate;
+
+        typedef double timetag;
+        typedef double flux;
+        typedef double efficiency;
+
+        std::ostream& write_binary(std::ostream& os, const coordinate& c);
+        std::istream& read_binary(std::istream &is, coordinate& c);
+
+        std::ostream& write_binary(std::ostream& os, const double& x);
+        std::istream& read_binary(std::istream &is, double& x);
+
+    };
+
+    namespace opt{
+
+        class Parameters{
+
+            public:
+
+                Parameters(int argc, char* argv[]);
+
+                GetOpt::GetOpt_pp *ops;
+                json params;
+
+                void enableConfig();
+                void enableHelp(std::string helpmessage);
+
+                template <typename T> T getOption(const char shortopt, const std::string longopt, T default_value){
+                    try{
+                        default_value = params[longopt];
+                    } catch (std::exception &e){}
+                    params[longopt] = ops->getopt<T>(shortopt, longopt, default_value);
+                    return params[longopt];
+                };
+
+                private:
+                    std::string filename;
+                    std::string execname;
+
+        };
+    };
+}
+
+#endif
---
+++ sim/src/sim_util.cpp
@@ -0,0 +1,88 @@
+#include "sim_util.hpp"
+
+namespace sim{
+
+    namespace log{
+    
+        void info(std::string msg) {
+            std::clog << msg;
+        }
+        void warn(std::string msg) {
+            std::cerr << msg;
+        }
+        void critical(std::string msg) {
+            std::cerr << msg; exit(EXIT_FAILURE);
+        }
+    };
+
+    namespace io{
+
+        std::ostream& write_binary(std::ostream& os, const coordinate& c){
+            const char *out_buffer = reinterpret_cast<const char *>(&c);
+            return os.write(out_buffer, sizeof(coordinate));
+        }
+
+        std::istream& read_binary(std::istream &is, coordinate& c){
+            char *in_buffer = reinterpret_cast<char *>(&c);
+            is.read(in_buffer, sizeof(coordinate));
+                if (!is){
+                    is.setstate(std::ios_base::failbit);
+                return is;
+            }
+            return is;
+        }
+
+        std::ostream& write_binary(std::ostream& os, const double& x){
+            const char *out_buffer = reinterpret_cast<const char *>(&x);
+            return os.write(out_buffer, sizeof(double));
+        }
+
+        std::istream& read_binary(std::istream &is, double& x){
+            char *in_buffer = reinterpret_cast<char *>(&x);
+            is.read(in_buffer, sizeof(double));
+                if (!is){
+                    is.setstate(std::ios_base::failbit);
+                return is;
+            }
+            return is;
+        }
+
+    }
+
+    namespace opt{
+
+        Parameters::Parameters(int argc, char* argv[]){
+            ops = new GetOpt::GetOpt_pp(argc, argv, GetOpt::Include_Environment);
+            filename = ops->getopt<std::string>('p', "PARAMETERS", "default.json");
+            std::ifstream param_file(filename, std::ifstream::in);
+            execname = argv[0];
+            
+            if (param_file) {
+                param_file >> params;
+                params = params[execname];
+            }
+        }
+        
+        void Parameters::enableConfig(){
+            bool config = false;
+                *ops >> GetOpt::OptionPresent('c', "config", config);
+                if (config){
+                    json config_param;
+                    config_param[execname] = params;
+                    std::cout << config_param.dump(4);
+                    exit(EXIT_SUCCESS);
+                }
+        }
+        
+        void Parameters::enableHelp(std::string helpmessage){
+            bool help = false;
+                *ops >> GetOpt::OptionPresent('h', "help", help);
+                if (help){
+                    log::info(helpmessage);
+                    exit(EXIT_SUCCESS);
+                }
+        }
+
+    };
+
+};
