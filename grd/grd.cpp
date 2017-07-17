f64519e78830538dc1e386b369e98d58a7ac8b5b
Till Zickmantel <zickmantel@physik.uni-luebeck.de>
2017-07-17 09:14:05 +0200

    Added 'grd', a 3D coordinate grid component.

---
+++ grd/grd.cpp
@@ -0,0 +1,49 @@
+#include "sim_util.hpp"
+
+const std::string helpmessage = 
+R"(
+Create a 3D evenly spaced grid of coordinates written in binary 16-bit integers to the standard output.
+
+Usage: grd [options]
+
+    -x --x-points : Number of points in the x dimension.
+    -y --y-points : Number of points in the y dimension.
+    -z --z-points : Number of points in the z dimension.
+
+    -X --x-limit : Maximal distance from origin in x in meters.
+    -Y --y-limit : Maximal distance from origin in y in meters.
+    -Z --z-limit : Maximal distance from origin in z in meters.
+
+    -p --PARAMETERS : Specify a parameter json file. Defaults to PARAMETERS environment variable or 'defaults.json'.
+    -c --config : Generate a config .json file and print it to the standard output.
+    -h --help : Show this help message.
+)";
+
+int main (int argc, char *argv[]){
+
+    int16_t x, y, z, X, Y, Z;
+
+    sim::opt::Parameters p{argc, argv};
+	x = p.getOption('x', "x-points", 100);
+	y = p.getOption('y', "y-points", 100);
+	z = p.getOption('z', "z-points", 100);
+	X = (int16_t) (1e+9*p.getOption('X', "x-limit", 1e-6));
+	Y = (int16_t) (1e+9*p.getOption('Y', "y-limit", 1e-6));
+	Z = (int16_t) (1e+9*p.getOption('Z', "z-limit", 1e-6));
+    p.enableConfig();
+    p.enableHelp(helpmessage);
+
+    sim::io::coordinate c{0, 0, 0};
+
+    for (int k=0; k<x; k++){
+        for (int j=0; j<y; j++){
+            for (int i=0; i<z; i++){
+                c.x = 2*X/x*k-X;
+                c.y = 2*Y/y*j-Y;
+                c.z = 2*Z/z*i-Z;
+                sim::io::write_binary(std::cout, c);
+            }
+        }
+    }
+
+}
