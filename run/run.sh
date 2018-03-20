9288ddb1290e9a743e69a7f2bcdd81630c8a4ec0
Till Zickmantel <till.zickmantel@gmail.com>
2018-03-20 09:23:36 +0100

    Improved run script.

--- run/run.sh
+++ run/run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-LOGFILE=$1.log
+LOGFILE=$1.log.json
 TMP=./tmp
 
 export SIM_PARAMS=@$1.json
@@ -17,20 +17,27 @@ EXI=../build/src/components/exi/sim_exi$SUFFIX
 PH2=../build/src/components/ph2/sim_ph2$SUFFIX
 DET=../build/src/components/det/sim_det$SUFFIX
 
-cd $TMP; mkfifo c_exi c_det exi emi; cd ..
+# Extracting the expected byte payload from diffusion
+TIME=$(jq < $1.json '.dif["experiment-time"]')
+INCREMENT=$(jq < $1.json '.dif["increment"]')
+PAYLOAD=$(awk '{printf "%d\n", ($1/$2*8*4)}' <(echo "$TIME $INCREMENT" | sed 's/\./,/g'))
+BYTES_PER_SECOND=$(awk '{printf "%d\n", ($1/$2*64)}' <(echo "1 $INCREMENT" | sed 's/\./,/g'))
+BUFFER_SIZE=100M
 
-# DRY RUN
-$DIF --iscope "/dif" --oscope "/dif/to_exi"                                                     --output $TMP/c_exi     --config $LOGFILE
-$DIF --iscope "/dif" --oscope "/dif/to_det"                                                     --output $TMP/c_det     --config $LOGFILE 
-$EXI --scope "/exi"                             --input $TMP/c_exi                              --output $TMP/exi       --config $LOGFILE 
-$PH2 --scope "/ph2"                                                                                                     --config $LOGFILE 
-$DET --scope "/det"                             --coordinates $TMP/c_det --photons $TMP/emi     --output $1.dat         --config $LOGFILE
+cd $TMP
+mkfifo c_exi
+mkfifo c_det_pre 
+mkfifo c_det
+mkfifo exi 
+mkfifo emi
+cd ..
 
 # SIMULATION
-$DIF --scope "/dif"                                                                             --output $TMP/c_exi     &
-$DIF --scope "/dif"                                                                             --output $TMP/c_det     &
-$EXI --scope "/exi"                             --input $TMP/c_exi                              --output $TMP/exi       &
-$PH2 --scope "/ph2"                                                                                                     &
-$DET --scope "/det"                             --coordinates $TMP/c_det --photons $TMP/emi     --output $1.dat
+$DIF --iscope "/dif" --oscope "/dif/to_exi"                                                      --output $TMP/c_exi     --log $LOGFILE    & sleep 0.1 
+$DIF --iscope "/dif" --oscope "/dif/to_emi"                                                      --output $TMP/c_det_pre --log $LOGFILE    & sleep 0.1
+pv < $TMP/c_det_pre > $TMP/c_det -N "completion" -i 0.25 -feprbcTS -s $PAYLOAD -B $BUFFER_SIZE &
+$EXI --scope "/exi"                             --input $TMP/c_exi                              --output $TMP/exi       --log $LOGFILE    & sleep 0.1
+$PH2 --scope "/ph2"                                                                                                     --log $LOGFILE    & sleep 0.1
+$DET --scope "/det"                             --coordinates $TMP/c_det --photons $TMP/emi     --output $1.dat         --log $LOGFILE 
 
 rm $TMP/*
